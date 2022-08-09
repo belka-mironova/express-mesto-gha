659fd748b171ca598a8faf3b95ce1f6e028b2cb7
Bella Mironova <bellamironova@gmail.com>
2022-08-09 12:35:44 +0300

    Added validation 3.0

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { celebrate, Joi } = require('celebrate');
+const { errors } = require('celebrate');
 
 const { login, createUser } = require('./src/controller/users');
 const auth = require('./src/middlewares/auth');
@@ -15,10 +17,25 @@ app.use(express.json());
 
 app.post(
   '/signin',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+    }).unknown(true),
+  }),
   login,
 );
 app.post(
   '/signup',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string().pattern(/(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/),
+    }).unknown(true),
+  }),
   createUser,
 );
 
@@ -27,6 +44,7 @@ app.use(auth);
 app.use('/users', userRoutes);
 app.use('/cards', cardRoutes);
 
+app.use(errors());
 app.use((req, res) => res.status(404).send({ message: 'Страницы не существует' }));
 
 const { PORT = 3000 } = process.env;
